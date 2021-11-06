33b707bf8b07754d6f5fdf220f6eaf1fa26b5ead
Mark <ratrateroo@gmail.com>
2021-11-06 15:30:20 +0800

    started building the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from 'react-router-dom';
+
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+
+import UserLogin from './user/pages/UserLogin';
+import UserSignupForm from './user/components/UserSignupForm';
+import UserProfile from './user/pages/UserProfile';
+//import UserBlogs from './user/pages/UserBlogs';
+import Users from './user/pages/Users';
+
+// import Blog from './blogs/pages/Blog';
+// import Blogs from './blogs/pages/Blogs';
+// import CreateBlog from './blogs/pages/CreateBlog';
+// import UpdateBlog from './blogs/pages/UpdateBlog';
+
+const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [token, setToken] = useState(null);
+	const [userId, setUserId] = useState(null);
+
+	const login = useCallback((token, userId, tokenExpiration) => {
+		setIsLoggedIn(true);
+		setToken(token);
+		setUserId(userId);
+	}, []);
+
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+		setToken(null);
+		setUserId(null);
+	}, []);
+
+	let routes = (
+		<Switch>
+			{/* <Route path="/" exact>
+				<Blogs title="Blogs" />
+			</Route> */}
+			{/* {token && <Redirect from="/signup" to="/" exact />} */}
+			{token && <Redirect from="/login" to="/users" exact />}
+			{!token && <Redirect from="/users" to="/login" exact />}
+			{/* {token && <Redirect to="/" />} */}
+			{token && (
+				<Route path="/users" exact>
+					<Users title="Users" />
+				</Route>
+			)}
+
+			{/* <Route path="/blogs/:uid" exact>
+				<UserBlogs title="User Blogs" />
+			</Route> */}
+			{/* <Route path="/blog/new" exact>
+				<CreateBlog title="Create Blog" />
+			</Route> */}
+			{/* <Route path="/blog/update/:bid" exact>
+				<UpdateBlog title="Update Blog" />
+			</Route> */}
+			{/* <Route path="/blog/:bid" exact>
+				<Blog title="<Username>'s Blog" />
+			</Route> */}
+			{isLoggedIn && (
+				<Route path="/profile/:uid" exact>
+					<UserProfile title="User Profile" />
+				</Route>
+			)}
+			<Route path="/login" exact>
+				<UserLogin title="User Login" />
+			</Route>
+			{!isLoggedIn && (
+				<Route path="/signup" exact>
+					<UserSignupForm title="User Signup" />
+				</Route>
+			)}
+			{!token && <Redirect from="/" to="/login" exact />}
+		</Switch>
+	);
+
+	return (
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: isLoggedIn,
+				token: token,
+				userId: userId,
+				login: login,
+				logout: logout,
+			}}>
+			<Router>
+				<MainNavigation />
+				{routes}
+			</Router>
+		</AuthContext.Provider>
+	);
+};
 
 export default App;
