e69a2e3c5ed51c64769ce0accc1aa09d7458e7d9
Mark <ratrateroo@gmail.com>
2021-11-10 14:22:36 +0800

    added auth provider and auth context

---
+++ client/src/shared/auth/AuthProvider.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+//import { fakeAuthProvider } from './fakeAuthProvider';
+
+//import { AuthContext } from './auth-context';
+const AuthProvider = ({ children }) => {
+	let [user, setUser] = React.useState(null);
+
+	let signin = (newUser, callback) => {
+		return fakeAuthProvider.signin(() => {
+			setUser(newUser);
+			callback();
+		});
+	};
+
+	let signout = (callback) => {
+		return fakeAuthProvider.signout(() => {
+			setUser(null);
+			callback();
+		});
+	};
+
+	let value = { user, signin, signout };
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+export default AuthProvider;
