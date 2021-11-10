2be104a3e5e197161e5da073779175fd254299c4
Mark <ratrateroo@gmail.com>
2021-11-10 14:36:36 +0800

    added require auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,38 @@
-import React, { useState, useCallback } from 'react';
+import React from 'react';
 import './App.css';
 import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 
-import { AuthContext } from './shared/context/auth-context';
+import AuthProvider from './shared/auth/AuthProvider';
 import Layout from './shared/components/Layout/Layout';
 import Users from './user/pages/Users';
 import UserLogin from './user/pages/UserLogin';
 import UserSignup from './user/pages/UserSignup';
 
 const App = () => {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [token, setToken] = useState(null);
-	const [userId, setUserId] = useState(null);
-
-	const login = useCallback((token, userId, tokenExpiration) => {
-		setIsLoggedIn(true);
-		setToken(token);
-		setUserId(userId);
-	}, []);
-
-	const logout = useCallback(() => {
-		setIsLoggedIn(false);
-		setToken(null);
-		setUserId(null);
-	}, []);
-
 	return (
-		<AuthContext.Provider
-			value={{
-				isLoggedIn: isLoggedIn,
-				token: token,
-				userId: userId,
-				login: login,
-				logout: logout,
-			}}>
+		<AuthProvider>
 			<Layout>
 				<Routes>
-					<Route path="/" element={<Navigate replace to="/users" />} />
-					<Route path="users" element={<Users />} />
+					<Route path="/" element={<Navigate replace to="/login" />} />
+					{/* <Route path="users" element={<Users />} /> */}
 					<Route path="login" element={<UserLogin />} />
+
 					<Route path="signup" element={<UserSignup />} />
+
+					<Route
+						path="/users"
+						element={
+							<RequireAuth>
+								<Users />
+							</RequireAuth>
+						}
+					/>
 					<Route path="*" element={<UserLogin />} />
 				</Routes>
 
 				<Outlet />
 			</Layout>
-		</AuthContext.Provider>
+		</AuthProvider>
 	);
 };
 
--- client/src/shared/auth/AuthProvider.js
+++ client/src/shared/auth/AuthProvider.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 //import { fakeAuthProvider } from './fakeAuthProvider';
 
-//import { AuthContext } from './auth-context';
+import { AuthContext } from './AuthContext';
 const AuthProvider = ({ children }) => {
-	let [user, setUser] = React.useState(null);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [token, setToken] = useState(null);
+	const [userId, setUserId] = useState(null);
 
-	let signin = (newUser, callback) => {
-		return fakeAuthProvider.signin(() => {
-			setUser(newUser);
-			callback();
-		});
+	const login = useCallback((token, userId, tokenExpiration) => {
+		setIsLoggedIn(true);
+		setToken(token);
+		setUserId(userId);
+	}, []);
+
+	const logout = useCallback(() => {
+		setIsLoggedIn(false);
+		setToken(null);
+		setUserId(null);
+	}, []);
+
+	value = {
+		isLoggedIn: isLoggedIn,
+		token: token,
+		userId: userId,
+		login: login,
+		logout: logout,
 	};
-
-	let signout = (callback) => {
-		return fakeAuthProvider.signout(() => {
-			setUser(null);
-			callback();
-		});
-	};
-
-	let value = { user, signin, signout };
 
 	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
